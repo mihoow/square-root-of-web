71bd5b9c83b0909aac63e5f1b094719142cbd3de
Michał Wieczorek <michal.wieczorek@scandiweb.com>
2024-04-11 14:29:34 +0200

    Add light version of logo for dark mode

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,27 @@
 import { Image } from './Image';
 import { Link } from '@remix-run/react';
+import { Theme } from '~/features/theme/config';
 import { ThemeToggler } from '~/features/theme/components/ThemeToggler';
 import { component } from '~/utils/component';
+import { useTheme } from '~/features/theme/contexts/Theme.context';
+
+const LogoImage = component('LogoImage', function ({ className }) {
+    const [theme] = useTheme()
+
+    const imgVariant = theme === Theme.DARK ? '-light' : ''
+
+    return (
+        <Image
+            source={[
+                { src: `/logo/square-root-32${imgVariant}.png`, width: 32, height: 32 },
+                { src: `/logo/square-root-48${imgVariant}.png`, width: 48, height: 48 },
+            ]}
+            alt='website logo'
+            loading='eager'
+            className={this.mcn(className)}
+        />
+    );
+});
 
 const Logo = component('Logo', function ({ className }) {
     return (
@@ -13,14 +33,7 @@ const Logo = component('Logo', function ({ className }) {
                 this.mod('hover', 'text-primary')
             )}
         >
-            <Image
-                source={[
-                    { src: '/logo/square-root-32.png', width: 32, height: 32 },
-                    { src: '/logo/square-root-48.png', width: 48, height: 48 },
-                ]}
-                alt='website logo'
-                loading='eager'
-            />
+            <LogoImage />
             <span
                 aria-hidden
                 className={this.cn('divider block h-10 w-[1px] bg-gray-300')}
@@ -34,12 +47,14 @@ const Logo = component('Logo', function ({ className }) {
 
 export const Header = component('Header', function ({ className }) {
     return (
-        <div className={this.mcn(
-            className,
-            'h-12 py-2 border-b-2 bg-white',
-            'md:h-20 md:py-4',
-            'dark:bg-darkBackground dark:border-primary'
-        )}>
+        <div
+            className={this.mcn(
+                className,
+                'h-12 py-2 border-b-2 bg-white',
+                'md:h-20 md:py-4',
+                'dark:bg-darkBackground dark:border-primary'
+            )}
+        >
             <div className={this.cn('contain h-full flex items-center justify-between')}>
                 <Logo />
                 <ThemeToggler className={this.cn('w-6 h-6', 'md:w-8 md:h-8')} />
